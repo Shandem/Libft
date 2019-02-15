366ebd478e3c7b75af272929ef3675ef47d60b72
Joshuah <josh@nMac.local>
2019-02-14 18:30:31 -0800

    added work from laptop

--- libft/srcs/ft_memccpy.c
+++ libft/srcs/ft_memccpy.c
@@ -13,3 +13,4 @@
 #include "libft.h"
 
 void	ft_memccpy(void dst, const void src, int c,  size_t n)
+{
--- libft/srcs/ft_memcpy.c
+++ libft/srcs/ft_memcpy.c
@@ -12,15 +12,14 @@
 
 #include "libft.h"
 
-void	ft_memcpy(void dst, const void src, size_t n)
+void	ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
+	unsigned char *d;
+	unsigend char *s;
 
-	i = 0;
-	while (i < n)
-	{
-		((char *) dst[i] = ((char *) src)[i]);
-		i++;
-	}
-	return (i);
+	d = (unsigned char*)dst;
+	s = (unsigned char*)src;
+	while (n--)
+		*d++ = *s++;
+	return(dst);
 }
