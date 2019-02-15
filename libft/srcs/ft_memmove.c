0a47dbb9f202165b937dc9ae0c8ab1cb066f9601
Joshuah Gutierrez <joshguti@e1z2r7p34.42.us.org>
2019-02-14 18:43:40 -0800

    added 643pm

--- libft/srcs/ft_memmove.c
+++ libft/srcs/ft_memmove.c
@@ -1,16 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joshguti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/14 15:35:15 by joshguti          #+#    #+#             */
-/*   Updated: 2019/02/14 18:32:20 by joshguti         ###   ########.fr       */
+/*   Created: 2019/02/14 18:35:43 by joshguti          #+#    #+#             */
+/*   Updated: 2019/02/14 18:43:09 by joshguti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft.f"
 
-void	ft_memccpy(void dst, const void src, int c,  size_t n)
+void	memmove(void *dst, const void *src, size_t len)
 {
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char*)dst;
+	s = (unsigned char*)src;
+
+	if (d == s)
+		return (dst);
+	if (s < d)
+	{
+		d += len - 1;
+		s += len - 1;
+		while (len--)
+			*d-- = *s--;
+	}
+	else
+		while (len--)
+			*d++ = *s++;
+	return (dst);
+}
