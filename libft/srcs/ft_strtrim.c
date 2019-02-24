197a9c009480ee38b9987cf22905b42d1360b2a7
Joshuah <josh@nMac.local>
2019-02-23 18:40:01 -0800

    added ftstrtrim to the list

--- libft/srcs/ft_strtrim.c
+++ libft/srcs/ft_strtrim.c
@@ -21,6 +21,15 @@ char	*ft_strtrim(char const *s)
 	if (!s)
 		return (NULL);
 	min = 0;
-	while (s[min] != '\0' && (s[min] == ' ' || 
-				//cfatrane
-				//
+	while (s[min] != '\0' && s[min] == ' ' || s[min] == '\n' ||
+		s[min] == '\t')
+		min++;
+	max = ft_strlen(s);
+	while (min < max && (s[max - 1] == ' ' || s[max - 1] == '\n' ||
+		s[max - 1] == '\t'))
+		max--;
+	if (min == max)
+		return (ft_strnew(1));
+	len = max - min;
+	return (ft_strsub(s, min len));
+}
