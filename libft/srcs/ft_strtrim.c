3ea65e2d94dcd3ae10a815f62b5664dd0f8e671d
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2019-02-26 10:17:27 -0800

    weekend work added

--- libft/srcs/ft_strtrim.c
+++ libft/srcs/ft_strtrim.c
@@ -1,35 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: joshguti <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/23 18:00:16 by joshguti          #+#    #+#             */
-/*   Updated: 2019/02/23 18:02:44 by joshguti         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+char  *ft_strtrim(char const *s)
 {
-	size_t min;
-	size_t max;
-	size_t len;
+  size_t start;
+  size_t len;
 
-	if (!s)
-		return (NULL);
-	min = 0;
-	while (s[min] != '\0' && s[min] == ' ' || s[min] == '\n' ||
-		s[min] == '\t')
-		min++;
-	max = ft_strlen(s);
-	while (min < max && (s[max - 1] == ' ' || s[max - 1] == '\n' ||
-		s[max - 1] == '\t'))
-		max--;
-	if (min == max)
-		return (ft_strnew(1));
-	len = max - min;
-	return (ft_strsub(s, min len));
+  start = 0;
+  while ((s[start]== ' ' || s[start] == '\n' || s[start] == '\t'))
+    start++;
+  if (s[start] == '\0')
+    return (ft_strdup(s + start));
+  len = ft_strlen(s) - 1;
+  while ((s[len] == ' ' || s[len] == '\n' || s[len] == '\t'))
+    len--;
+  return (ft_strsub(s, start, len - start + 1));
 }
