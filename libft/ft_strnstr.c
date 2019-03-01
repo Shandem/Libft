5d0c949357d632491607521f7666934ef771320d
Joshuah Gutierrez <joshguti@e1z3r5p4.42.us.org>
2019-03-01 14:50:50 -0800

    more corrections

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,28 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joshguti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/01 12:56:13 by joshguti          #+#    #+#             */
+/*   Updated: 2019/03/01 13:04:50 by joshguti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char      *ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-      int       lcpy;
-      char  *h;
-      char *n;
+	int			lcpy;
+	char	*h;
+	char	*n;
 
-      if (*haystack == *needle && *haystack == '\0')
-        return ((char*)haystack);
-      while (*haystack && len > 0)
-      {
-          lcpy = len;
-          h = (char *)haystack;
-          n = (char *)needle;
-          while (*n && *haystack && *n == *haystack && lcpy > 0)
-          {
-              n++;
-              haystack++;
-              lcpy--;
-          }
-          if (!*n)
-            return (h);
-          haystack = h + 1;
-          len--;
-      }
-      return (NULL);
+	if (*haystack == *needle && *haystack == '\0')
+		return ((char*)haystack);
+	while (*haystack && len > 0)
+	{
+		lcpy = len;
+		h = (char*)haystack;
+		n = (char*)needle;
+		while (*n && *haystack && *n == *haystack && lcpy > 0)
+		{
+			n++;
+			haystack++;
+			lcpy--;
+		}
+		if (!*n)
+			return (h);
+		haystack = h + 1;
+		len--;
+	}
+	return (NULL);
 }
