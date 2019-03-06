a7b81656ed13fa60c598c8edc8c4ff4e65e8bde1
Joshuah Gutierrez <joshguti@e1z2r8p7.42.us.org>
2019-03-06 12:18:55 -0800

    added work

--- libft/src/ft_memcpy.c
+++ libft/src/ft_memcpy.c
@@ -5,21 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: joshguti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/14 15:29:44 by joshguti          #+#    #+#             */
-/*   Updated: 2019/02/26 13:33:17 by joshguti         ###   ########.fr       */
+/*   Created: 2019/03/06 12:05:44 by joshguti          #+#    #+#             */
+/*   Updated: 2019/03/06 12:10:00 by joshguti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *restrict d, const void *restrict s, size_t n)
 {
-	unsigned char *d;
-	unsigned char *s;
+	unsigned char				*dst;
+	unsigned const char	*src;
+	size_t							count;
 
-	d = (unsigned char*)dst;
-	s = (unsigned char*)src;
-	while (n--)
-		*d++ = *s++;
+	dst = d;
+	src = s;
+	count = -1;
+	while (++count < n)
+		dst[count] = s[count];
 	return (dst);
 }
--- libft/src/ft_memmove.c
+++ libft/src/ft_memmove.c
@@ -5,31 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: joshguti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/14 18:35:43 by joshguti          #+#    #+#             */
-/*   Updated: 2019/03/04 15:59:08 by joshguti         ###   ########.fr       */
+/*   Created: 2019/03/06 11:14:38 by joshguti          #+#    #+#             */
+/*   Updated: 2019/03/06 11:16:44 by joshguti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*d;
-	unsigned char	*s;
-
-	d = (unsigned char*)dst;
-	s = (unsigned char*)src;
-	if (d == s)
-		return (dst);
-	if (s < d)
-	{
-		d += len - 1;
-		s += len - 1;
+	if (src <= dst)
 		while (len--)
-			*d-- = *s--;
-	}
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
 	else
-		while (len--)
-			*d++ = *s++;
-	return (dst);
+		ft_memcpy(dst, src, len);
+	return ((void *)dst);
 }
