4c2996dd9ea8e3b8eab2ae6bc34d647fead4ec6b
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2019-03-08 10:31:03 -0800

    strsplit fixed

--- libft/src/ft_strsplit.c
+++ libft/src/ft_strsplit.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	**ft_strsplit(char const *str, char delim)
+{
+	char	**out;
+	int	oi;
+	int	i;
+	int	start;
+
+	if (!str || !(out = (char**)malloc(sizeof(char*) * (ft_wordcount(str, delim)) + 1)))
+		return (NULL);
+	oi = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] == delim)
+			i++;
+		start = i;
+		while (str[i] && str[i] != delim)
+			i++;
+		if (i > start)
+			out[oi++] = ft_strndup(str + start, i - start);
+	}
+	out[oi] = NULL;
+	return (out);
+}
