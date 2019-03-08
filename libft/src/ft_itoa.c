f771131d76adb5df6f1d2f3e654a1520a1ac0e98
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2019-03-08 11:25:58 -0800

    re check my errors

--- libft/src/ft_itoa.c
+++ libft/src/ft_itoa.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_itoa(int n)
+{
+	char	*out;
+	int	ncpy;
+	int	w;
+	int	sign;
+
+	ncpy = n;
+	w = 1;
+	while ((ncpy /= 10))
+		w++;
+	sign = n < 0 ? 1 : 0;
+	out = ft_strnew(w + sign);
+	if (out == NULL)
+		return (NULL);
+	if (sign)
+		out[0] = '-';
+	while (w--)
+	{
+		out[sign + w] = '0' + (sign ? -(n % 10) : (n % 10));
+		n /= 10;
+	}
+	return (out);
+}
